b325e8c13b4d5beea02cb621637122a6e86ae0f2
Marwan <chaerul.marwan@weekendinc.com>
2022-08-09 10:43:30 +0700

    add default value

--- src/Customer.php
+++ src/Customer.php
@@ -5,7 +5,7 @@ namespace ARL\Library;
 class Customer {
   public function __construct(private string $name) {}
 
-  public function sayHello(string $name): string {
+  public function sayHello(string $name = "Guest"): string {
     return "Hello $name, My Name is $this->name";
   }
 }
