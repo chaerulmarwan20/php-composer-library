7b73248cd33aa4477714ed6a70abc6e918db70cf
Marwan <chaerul.marwan@weekendinc.com>
2022-08-09 10:33:29 +0700

    initial commit

---
+++ src/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ARL\Library;
+
+class Customer {
+  public function __construct(private string $name) {}
+
+  public function sayHello(string $name): string {
+    return "Hello $name, My Name is $this->name";
+  }
+}
